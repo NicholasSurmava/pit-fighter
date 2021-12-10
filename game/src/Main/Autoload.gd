d47bf333bb49335e157b8620c802ba3d30f811e7
Nicholas Surmava <nicholas.surmava@gmail.com>
2021-12-09 19:08:12 -0500

    created enemy count generator and ad added to Autoload singleton for processing

--- game/src/Main/Autoload.gd
+++ game/src/Main/Autoload.gd
@@ -1,3 +1,4 @@
 extends Node
 
 var SCORE_SYSTEM = 0
+var enemies_per_level
--- game/src/Main/Game.gd
+++ game/src/Main/Game.gd
@@ -1,4 +1,24 @@
 extends Node
 
+func _ready() -> void:
+	# TODO: Randomly determine the number of enemies per level. 5-15 enemies.
+	# Add to Autoload script as singleton.
+	
+	# The spawners can then grab and spawn until they hit the limit.
+	var enemies_per_level = random_enemy_generator(4)
+	Autoload.enemies_per_level = enemies_per_level
+	return
+
 func _process(delta):
 	$CanvasLayer/Score.text = str(Autoload.SCORE_SYSTEM)
+
+func random_enemy_generator(levels):
+	# Takes in the number of levels and generators an int based on the number of levels passed in.
+	var enemies_to_generate = {}
+	
+	for level in levels:
+		randomize()
+		var number_of_enemies = randi() % 10 + 5
+		enemies_to_generate["level_" + str(level + 1)] = number_of_enemies
+		
+	return enemies_to_generate
